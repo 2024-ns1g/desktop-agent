7478922d66dca891ba2ffc141ccd1180a75e89da
rca <nem@nem.x0.to>
2025-02-05 18:08:34 +0900

    add some events

--- src/models/websocket.rs
+++ src/models/websocket.rs
@@ -23,10 +23,38 @@ pub struct ChangeCurrentPageData {
 }
 
 #[derive(Deserialize)]
+pub struct TriggerNextStepData {
+    #[serde(rename = "isPageChanged")]
+    pub is_page_changed: bool,
+    #[serde(rename = "newPageIndex")]
+    pub new_page_index: usize,
+    #[serde(rename = "newStepIndex")]
+    pub new_step_index: usize,
+}
+
+#[derive(Deserialize)]
+pub struct TriggerPrevStepData {
+    #[serde(rename = "isPageChanged")]
+    pub is_page_changed: bool,
+    #[serde(rename = "newPageIndex")]
+    pub new_page_index: usize,
+    #[serde(rename = "newStepIndex")]
+    pub new_step_index: usize,
+}
+
+#[derive(Deserialize)]
 #[serde(tag = "requestType")]
 pub enum WsEvent {
     #[serde(rename = "CHANGE_CURRENT_PAGE")]
     ChangeCurrentPage {
         data: ChangeCurrentPageData,
+    },
+    #[serde(rename = "TRIGGER_NEXT_STEP")]
+    TriggerNextStep {
+        data: TriggerNextStepData,
+    },
+    #[serde(rename = "TRIGGER_PREV_STEP")]
+    TriggerPrevStep {
+        data: TriggerPrevStepData,
     },
 }
